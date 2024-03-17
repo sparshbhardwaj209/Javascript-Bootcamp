e7a51397630b3564679b8b6d4d00671dcb2c91b2
Sparsh Bhardwaj <sparshbhardwaj209@gmail.com>
2024-03-18 01:31:00 +0530

    what is Javascript, variables, keywords and different types of Data Types

---
+++ 1variables.js
@@ -0,0 +1,23 @@
+// First Method:- using var
+
+var score = 90;
+var marks = 101;
+
+// Second Method:- using let
+
+let age = 23;
+let flag = 1;
+
+// Third Method:- using const
+
+const x = 100;
+const y = 50;
+
+console.log(10);
+console.log("sparsh");
+console.log(score);
+
+console.log(x, y);
+
+const z = undefined;
+console.log(z);
---
+++ 2values.js
@@ -0,0 +1,24 @@
+// Different types of values we can store in JS is:-
+
+/****
+ *
+ * 1. Number: 10, 100, 1000, -2, 3.6
+ *    Eg: var x = 10;
+ *
+ * 2. String: "Sparsh", 'sparsh', `sparsh`
+ *    Eg: var name = "Sparsh"
+ *
+ * 3. Boolean: true - 1, false - 0
+ *    Eg: var condition = true;
+ *
+ * 4. Undefined: Something which is not defined yet but may be defined in the future.
+ *    Eg: var status = undefined;
+ * 
+ * 5. Objects: <key, value>
+ *    Eg: {
+ *              name: "Sparsh",
+ *              company: "Nowhere",
+ *              positionWants: "Software Development Engineer"
+ *        }
+ *
+ */
